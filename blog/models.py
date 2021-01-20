e35e4f8f8ff1b5d6f35e5fd4e7f5323c9fc289f3
andreesnavarroo <andreesnavarroo@gmail.com>
2021-01-20 14:40:28 -0500

    primer commit (modelos)

---
+++ blog/models.py
@@ -0,0 +1,133 @@
+from django.db import models
+# Lib seguimiento de los cambios en los modelos
+from simple_history.models import HistoricalRecords
+
+from usuarios.models import Autor
+
+class BaseModel(models.Model):
+    """ BaseModel Funciona como una clase base abstracta
+    en la que los otros  modelos del proyecto heredarán los atributos de esta
+    clase, atributos:
+        + fecha_creacion (DateTime):almacena la fecha y hora en que se creó el objeto.
+        + fecha_modificacion (DateTime): almacena la última fecha y hora en que se modificó el objeto.
+        + fecha_eliminacion (DateTime): almacena la última fecha y hora en que se eliminó el objeto.
+        + estado (Bool): representa el estado
+    """
+
+    fecha_creacion = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Fecha y hora en la que se creó el objeto.'
+    )
+    fecha_modificacion = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        help_text='Fecha y hora en la que se modificó por última vez el objeto.'
+    )
+
+    fecha_eliminacion = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text='Fecha y hora en la que se eliminó el objeto.'
+    )
+
+    estado = models.BooleanField(default=True)
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
+
+        get_latest_by = 'fecha_creacion'
+        ordering = ['-fecha_creacion', '-fecha_modificacion']
+
+
+# Modelo Categorias
+class Categoria(BaseModel):
+    nombre = models.CharField(
+        'Nombre de la Categoría', 
+        max_length = 100, 
+        unique = True
+    )
+    historical = HistoricalRecords()
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.nombre
+
+
+# Modelo Post
+class Post(BaseModel):    
+    autor = models.ForeignKey(Autor, related_name = 'autor_post',
+            on_delete = models.CASCADE, null=False, blank = False)      
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE, related_name="categoria_post")
+    titulo = models.CharField(
+        'Titulo del post',
+        unique=True,
+        max_length=200,
+        blank=False, 
+        null=False
+    )
+    descripcion = models.CharField(
+        'Descipción del post',
+        max_length=500,
+        blank=False,
+        null=False
+    )
+    imagen = models.ImageField(
+        'imagen del post',
+        upload_to='blog/images/', 
+        blank=True, 
+        null=True
+    )
+    contenido = models.TextField('Contenido')
+    fecha_publicacion = models.DateField('fecha publicacion del post')
+    fecha_desactiva = models.DateField('fecha desactiva post')
+    publicado = models.BooleanField(help_text="Poner en True cuando el post este publicado", default=False)
+    historical = HistoricalRecords()
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return "Id: {} | Titulo: {}".format(self.pk, self.titulo)     
+
+
+
+# Modelo Likes
+class Like(BaseModel):
+
+    post = models.ForeignKey(Post, related_name = 'post_like',
+            on_delete = models.CASCADE, null=False, blank = False)
+    autor = models.ForeignKey(Autor, related_name = 'autor_like',
+            on_delete = models.CASCADE, null=False, blank = False)            
+    historical = HistoricalRecords()
+    class Meta:
+        verbose_name = 'Like'
+        verbose_name_plural = 'Likes'
+
+    def __str__(self):
+        return self.estado
+
+# Modelo Comentarios
+class Comentario(BaseModel):
+    post = models.ForeignKey(Post, related_name = 'post_comentario',
+            on_delete = models.CASCADE, null=False, blank = False)    
+    autor = models.ForeignKey(Autor, related_name = 'autor_like',
+            on_delete = models.CASCADE, null=False, blank = False)      
+    descripcion = models.CharField(
+        'Descipción del comentario',
+        max_length=500,
+        blank=False,
+        null=False
+    )
+            
+
+    class Meta:
+        verbose_name = 'Comentario'
+        verbose_name_plural = 'Comentarios'
+
+    def __str__(self):
+        return self.descripcion            
---
+++ usuarios/models.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+# Lib seguimiento de los cambios en los modelos
+from simple_history.models import HistoricalRecords
+
+# Modelo User
+class User(AbstractUser):
+    TIPO_USUARIO = (
+        ('ADMINISTRADOR','ADMINISTRADOR'),
+        ('USUARIO','USUARIO'),
+    )
+
+    tipo_usuario = models.CharField(max_length=25, choices=TIPO_USUARIO, default='ADMINISTRADOR')
+    nombre = models.CharField(max_length=50, null=False)
+    apellidos = models.CharField(max_length=50, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=False)
+
+    historical = HistoricalRecords()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+
+#model administradores
+class Autor(User):
+    imagen = models.ImageField(upload_to='autor/%Y/%m/%d', null=True, blank=True)
+    historical = HistoricalRecords()
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+        default_permissions = ()
+
+
+    def __str__(self):
+        return str(self.nombre + " " + self.apellidos)
+        
